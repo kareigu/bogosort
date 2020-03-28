476ee660036b26e89018f9cbb6c56911b61b4753
mxrr <datmixor@gmail.com>
2020-03-29 00:17:13 +0200

    Fixes and added ability to input array length

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,21 @@ use rand::seq::SliceRandom;
 use stopwatch::{Stopwatch};
 
 fn main() {
-    let sorted: Vec<i32> = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+    println!("Input array length: ");
+    let mut buf: String = "".to_string();
+
+    std::io::stdin().read_line(&mut buf).expect("Did not enter a correct string");
+    let sortlength = buf.trim().parse::<i32>();
+
+    let mut sorted: Vec<i32> = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+    match sortlength {
+        Ok(ok) => if ok > 0 { sorted = (1 .. (ok + 1)).map(i32::from).collect() },
+        Err(e) => println!("{}", e),
+    }
+
+
+
     let mut sortable: Vec<i32> = sorted.to_vec();
     let mut iterations: i128 = 0;
 
@@ -26,6 +40,9 @@ fn main() {
 
     let secondspassed: f64 = sw.elapsed_ms() as f64 / 1000 as f64;
     println!("Seconds: {}", secondspassed);
+
+
+    std::io::stdin().read_line(&mut buf).expect("Did not enter a correct string");
 }
 
 fn bogosort(iterations: &mut i128, sorted: &Vec<i32>, unsorted: &mut Vec<i32>) {
